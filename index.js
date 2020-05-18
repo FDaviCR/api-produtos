fd84f3db7aa77dae4c22f329883c1b5b3a3026db
Davi <davicamiloribeiro@gmail.com>
2020-05-18 20:47:56 -0300

    GET por id

--- index.js
+++ index.js
@@ -11,8 +11,28 @@ app.listen(3000, () => {
     console.log("O servidor está rodando!")
 });
 
-app.get('/produtos', (req, res) => {
+app.get('/produtos', (req, res) => { 
     connection.query('select * from produtos', (err, data) => {
-        res.json(data);
+        if(!err){
+            res.statusCode = 200;
+            res.json(data);
+        }else{
+            console.log(err);
+        }
     })
+});
+
+app.get('/produtos/:id', (req, res) => { 
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);
+    }else{
+        connection.query('select * from produtos where id = ?',[req.params.id], (err, data) => {
+            if(!err){
+                res.statusCode = 200;
+                res.json(data);
+            }else{
+                res.send(err);
+            }
+        })
+    }  
 });
