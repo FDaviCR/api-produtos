0f08c7b6e5ab88175624bd32a522cd6d80e12a3d
Davi <davicamiloribeiro@gmail.com>
2020-05-25 23:39:00 -0300

    Verificando token valido na requisição

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 const express = require('express');
 const app = express();
+const session = require("express-session");
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 
-const JWTSecret = 'YouShallNotPass';
+const JWTSecret = "YouShallNotPass";
 
 app.use(cors());
-
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+function auth(req, res, next){
+    const authToken = req.headers['authorization'];
+
+    if(authToken != undefined){
+        const bearer = authToken.split(' ');
+        var token = bearer[1];
+        //console.log(token);
+
+        jwt.verify(token, JWTSecret,(err, data) => {
+            if(err){
+                res.status(401);
+                res.json({err:"Token inválido!"});
+            }else{
+                req.token = token;
+                req.loggedUser = {id: data.id, login: data.login};
+                next();
+            }
+        });
+    }else{
+        res.status(401);
+        res.json({err:"Token inválido!"});
+    }
+}
 
 const connection = require("./database/database");
 
@@ -17,7 +41,7 @@ app.listen(3000, () => {
     console.log("O servidor está rodando!")
 });
 
-app.get('/produtos', (req, res) => { 
+app.get('/produtos', auth, (req, res) => { 
     connection.query('select * from produtos', (err, data) => {
         if(!err){
             res.statusCode = 200;
